2b7d8bf7be137b6f115816eeb8322e2487e17e3d
VitorNunes86 <145569863+VitorNunes86@users.noreply.github.com>
2025-09-09 10:26:05 -0300

    09/10

--- main.js
+++ main.js
@@ -68,11 +68,11 @@ function geraSenha() {
 
 function classificaSenha() {
     forcasenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha > 11) {
+    if (tamanhoSenha > 11){
         forcaSenha.classList.add('forte');
     } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 5) {
+    } else if (tamanhoSenha <= 5){
         forcaSenha.classList.add('fraca');
     }
 }
